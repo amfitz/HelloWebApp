07e244c940ab8cac2e8fdcafa86a9025dc232ef4
amfitz <lexilui@hotmail.com>
2016-07-01 16:46:57 -0700

    added templates

---
+++ hellowebapp/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, patterns, include
+from django.contrib import admin
+from collection import views
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    url(r'^$', views.index, name="home"),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html"), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name="contact.html"), name="contact"),
+    url(r'^admin/', include(admin.site.urls)),
+]
